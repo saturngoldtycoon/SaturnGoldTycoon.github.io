bf3ee0e5eb99b346a00c51ae0567c8092cdf6391
saturngoldtycoon <ronoldyoung@gmail.com>
2026-06-19 16:30:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-// 1. Grab the elements from your HTML page
-var osDropdown = document.getElementById('os');
-var issueDropdown = document.getElementById('issue');
-var priceLabel = document.getElementById('total-price');
+window.addEventListener('DOMContentLoaded', () => {
+    var osSelect = document.getElementById('os');
+    var issueSelect = document.getElementById('issue');
+    var totalDisplay = document.getElementById('total-price');
 
-// 2. Create the function that does the math
-function calculate() {
-    var osPrice = Number(osDropdown.value);
-    var issuePrice = Number(issueDropdown.value);
-    var finalTotal = osPrice + issuePrice;
-    
-    priceLabel.innerText = "Estimated Cost: $" + finalTotal;
-}
+    function updateEstimate() {
+        var baseCost = Number(osSelect.value);
+        var addedCost = Number(issueSelect.value);
+        var finalPrice = baseCost + addedCost;
+        
+        totalDisplay.innerText = "$" + finalPrice;
+    }
 
-// 3. Tell both dropdowns to run the math function whenever they change
-osDropdown.addEventListener('change', calculate);
-issueDropdown.addEventListener('change', calculate);
+    osSelect.addEventListener('change', updateEstimate);
+    issueSelect.addEventListener('change', updateEstimate);
+});
