6ca48ad6adfa4d766cd2861d6a8ac2a53a0f6d43
saturngoldtycoon <ronoldyoung@gmail.com>
2026-07-03 11:20:35 -0400

    Refactor estimate calculation and remove osSelect

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
-    var osSelect = document.getElementById('os');
     var issueSelect = document.getElementById('issue');
     var totalDisplay = document.getElementById('total-price');
     var quoteForm = document.getElementById('quote-form');
 
+    var TRAVEL_FEE = 25;
+
     function updateEstimate() {
-        var baseCost = Number(osSelect.value);
-        var addedCost = Number(issueSelect.value);
-        var finalPrice = baseCost + addedCost;
+        var issueCost = Number(issueSelect.value);
+        var finalPrice = issueCost + TRAVEL_FEE;
         totalDisplay.innerText = "$" + finalPrice;
     }
 
-    osSelect.addEventListener('change', updateEstimate);
     issueSelect.addEventListener('change', updateEstimate);
 
     // Fill and submit
     quoteForm.addEventListener('submit', function(e) {
         e.preventDefault();
-        
+
         // Set hidden field values
-        document.getElementById('hidden-os').value = osSelect.selectedOptions[0].text;
         document.getElementById('hidden-issue').value = issueSelect.selectedOptions[0].text;
         document.getElementById('hidden-total').value = totalDisplay.innerText;
-        
+
         // Submit form after values are set
         setTimeout(function() {
             quoteForm.submit();
